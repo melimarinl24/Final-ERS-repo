3556eef9311df0f528253b0bc1f9afe0e8ce52c3
Elysia Roncal <181039429+88lile@users.noreply.github.com>
2025-12-06 02:50:44 -0800

    Remove migrate.init_app call (no Flask-Migrate in prod)

--- project/__init__.py
+++ project/__init__.py
@@ -54,7 +54,7 @@ def create_app():
     # --------------------------
     db.init_app(app)
     csrf.init_app(app)
-    migrate.init_app(app, db)
+    # migrate.init_app(app, db)
     login_manager.init_app(app)
 
     # --------------------------
